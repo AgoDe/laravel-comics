70a9260bea51d3a8e6459065db854bae99ed835c
AgoDe <ago.defelice@gmail.com>
2022-03-03 19:01:29 +0100

    add comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,1 +1,22 @@
 @extends('layouts.default')
+
+@section('section-1')
+
+<div class="card-container container">
+
+    <div class="article-title"> CURRENT SERIES</div>   
+
+    <div class="flex-box">
+        <!-- stampa dell'array dei fumetti -->
+        @foreach ($comics as $item)
+        <div>  
+            <img src="{{$item['thumb'] }}" alt="">   
+            <h4> {{ $item['title'] }} </h4>
+        </div>
+        @endforeach
+    </div>
+
+    <div class="load-button">LOAD MORE</div> 
+
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/comics', function () {
-    return view('comics');
+    $comics = config('comics');
+
+    return view('comics', compact('comics'));
 })->name('comics');
