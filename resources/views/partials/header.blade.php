359aed059ae8c414bc24cac76548177d1f0d0419
AgoDe <ago.defelice@gmail.com>
2022-03-03 18:30:09 +0100

    fix header (link menu) & add comics page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,20 +6,62 @@
 </div>
 <div class="navbar container">
     
-    <img src=" {{ asset('images/dc-logo.png') }}" alt="logo">
+    <a href=" {{ route('home')}}">
+        <img src=" {{ asset('images/dc-logo.png') }}" alt="logo">
+    </a>
     
     
     <ul>
-        <li>characters</li>
-        <li>comics</li>
-        <li>movies</li>
-        <li>tv</li>
-        <li>games</li>
-        <li>collectibles</li>
-        <li>videos</li>
-        <li>fans</li>
-        <li>news</li>
-        <li>shop</li>
+        <li>
+            <a href="#">
+                characters
+            </a>
+        </li>
+        <li>
+            <a href=" {{ route('comics') }} ">
+                comics
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                movies
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                tv
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                games
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                collectibles
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                videos
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                fans
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                news
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                shop
+            </a>
+        </li>
     </ul>
    
 
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/comics', function () {
+    return view('comics');
+})->name('comics');
